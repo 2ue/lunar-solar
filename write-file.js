705d110e165e59cb8346185acb1d78ede234c89e
2ue <jie7466358356@153.com>
2018-01-06 15:50:03 +0800

    精简数据

--- write-file.js
+++ write-file.js
@@ -2,15 +2,16 @@
  * Created by 2ue on 2018/1/5.
  */
 
-const fs = require('fs')
-const path = require('path')
-// const resolveHtml = require('./resolve-html')
-const {endYear} = require('./config');
+const fs = require('fs');
+const path = require('path');
+const { startYear, endYear } = require('./config');
+
+const mergeData = require('./merge-date');
 
 //写入数据
-function writeFile(data,year,cb) {
+function writeFile(data, year, cb) {
 
-    const dataPath = path.relative(__dirname, './date-year'), targetPath = path.join(dataPath,`/${year}.json`);
+    const dataPath = path.relative(__dirname, './date-year'), targetPath = path.join(dataPath, `/${year}.json`);
 
     //dataPath目录是否存在，一个可写入的流
     let dataPathIsExists, writerStream;
@@ -27,7 +28,7 @@ function writeFile(data,year,cb) {
             }
             console.log(`=====${year} 目录创建成功！${year} =====`);
         });
-    };
+    }
 
     //创建一个可写入的流
     writerStream = fs.createWriteStream(targetPath);
@@ -39,19 +40,16 @@ function writeFile(data,year,cb) {
         console.log(err.stack);
         console.log(`=====${year} 数据写入失败！ ${year}=====`);
     });
-    console.log(endYear)
-    // if(year < endYear) setTimeout(function () {
-    //     resolveHtml(year+1,function (res,year) {
-    //         writeFile(res,year);
-    //     });
-    // },1500);
-    if(year < endYear) setTimeout(function () {
-        cb(year+1);
-    },1500);
+    if (year < endYear) {
+        setTimeout(function () {
+            cb(year + 1);
+        }, 1500);
+    } else {
+        console.log('====> 开始汇总数据任务 =====>')
+        setTimeout(function () {
+            mergeData(startYear, endYear);
+        },2000)
+    }
 }
-
-// resolveHtml(startYear,function (res,year) {
-//     writeFile(res,year);
-// })
 
 module.exports = writeFile;
