ac6011c40b0af728f59e612e90be9aba55291699
2ue <jie746635835@163.com>
2019-02-28 10:31:56 +0800

    修改目录

---
+++ spider/index.js
@@ -0,0 +1,78 @@
+/**
+ * Created by 2ue on 2018/1/4.
+ * 解析采集到的数据按照格式组装
+ */
+
+//lunar农历，阴历
+//solar阳历，太阳历
+//divisions节气，分支
+
+const request = require('request');
+const iconv = require('iconv-lite');
+const writeFile = require('./write-file');
+let { startYear, lunarYear, lunarMon, nmCN, lunarCN, divisionsTr } = require('../utils/config');
+
+//把旧历汉字转换成数字
+function countOldToNum(text) {
+    if (!text) return 0;
+    let num = text.length > 1 ? text.split('').reduce((prev, curr) => lunarCN.indexOf(prev) * 10 + nmCN.indexOf(curr) + 1) : (nmCN.indexOf(text) + 1);
+
+    return num > 30 ? 30 : num;
+}
+
+function getUrl(year) {
+    return `http://data.weather.gov.hk/gts/time/calendar/text/T${year}c.txt`;
+};
+
+//解析页面
+function getHtml(year) {
+    if (!year) return [];
+    return new Promise(function (resolve, reject) {
+        request({ url: getUrl(year), encoding: null }, function (error, response, body) {
+            console.log(`=== 开始抓取 ===>  ${getUrl(year)}`);
+            if (!error && response.statusCode === 200) {
+                resolve(iconv.decode(body, 'big5').toString());
+            } else {
+                reject(error);
+            }
+        });
+    })
+}
+
+function index(year) {
+    getHtml(year).then(function (data) {
+        let result = {};
+        let arr = JSON.parse(JSON.stringify(data.split(/\n/)).replace(/\s+/g, ' ').replace(/\r/g, '').replace(/二十/g, lunarCN[ 1 ] + lunarCN[ 1 ]).replace(/正/g, nmCN[ 0 ]));//将汉字正月中正转换成一，以便正确计算
+
+        //去除开始和尾部的多余数据
+        arr = arr.splice(3, arr.length - 5);
+        arr.map(function (item) {
+            if (item && item.replace(/\s/g, '')) {
+                let info = item.split(' '), temp = true, solar = info[ 0 ].split(/[^0-9]/), lunar = info[ 1 ],
+                    divisions = divisionsTr.indexOf(info[ 3 ]) + 1;
+
+                //判断是否带月，带月表示当前月第一天
+                if (lunar.indexOf('月') > 0) {
+                    //解析旧历月份
+                    lunarMon = countOldToNum(lunar.replace('月', ''));
+                    //月份为一时，说明是下一年，年份加一
+                    //防止润一月时，lunarYear重复加
+                    if (lunarMon === 1 && temp) {
+                        lunarYear = lunarYear + 1;
+                        temp = false;
+                    }
+                    lunar = nmCN[ 0 ];
+                }
+                solar = solar.splice(0, solar.length - 1).join('-');
+                lunar = [ lunarYear, lunarMon, countOldToNum(lunar) ].join('-');
+                result[ solar ] = { s: solar, l: lunar };
+                if (divisions) result[ solar ].d = divisions;
+            }
+        });
+        writeFile(result, year, function (nextYear) {
+            index(nextYear)
+        });
+    });
+}
+
+index(startYear);
---
+++ spider/write-file.js
@@ -0,0 +1,55 @@
+/**
+ * Created by 2ue on 2018/1/5.
+ * 用于采集数据的单个方法
+ */
+
+const fs = require('fs');
+const path = require('path');
+const { startYear, endYear } = require('../utils/config');
+
+const mergeData = require('./merge-date');
+
+//写入数据
+function writeFile(data, year, cb) {
+
+    const dataPath = path.relative(__dirname, './date-year'), targetPath = path.join(dataPath, `/${year}.json`);
+
+    //dataPath目录是否存在，一个可写入的流
+    let dataPathIsExists, writerStream;
+
+    //判断dataPath目录是否存在
+    dataPathIsExists = fs.existsSync(dataPath);
+
+    //如果不存在就创建
+    if (!dataPathIsExists) {
+        fs.mkdir(dataPath, function (err) {
+            if (err) {
+                console.log(`=====${year} 目录创建失败！${year} =====`);
+                return console.error(err);
+            }
+            console.log(`=====${year} 目录创建成功！${year} =====`);
+        });
+    }
+
+    //创建一个可写入的流
+    writerStream = fs.createWriteStream(targetPath);
+    writerStream.write(JSON.stringify(data, null, 4), 'utf-8');
+    writerStream.end();//标记结束
+    writerStream.on('finish', function () {
+        console.log(`=====${year} 数据写入完成！ ${year}=====`);
+    }).on('error', function (err) {
+        console.log(err.stack);
+        console.log(`=====${year} 数据写入失败！ ${year}=====`);
+    });
+    if (year < endYear) {
+        setTimeout(function () {
+            cb(year + 1);
+        }, 1500);
+    } else {
+        console.log('====> 开始汇总数据任务 =====>')
+        setTimeout(function () {
+            mergeData(startYear, endYear);
+        },2000)
+    }
+}
+module.exports = writeFile;
